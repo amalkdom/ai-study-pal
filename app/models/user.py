a3a760d9dee1545c08b8d32860c20cedd225983f
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-02-28 18:21:16 +0530

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,23 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    age = db.Column(db.Integer)
+    student_class = db.Column(db.String(50))
+    email = db.Column(db.String(150), unique=True)
+    password_hash = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
