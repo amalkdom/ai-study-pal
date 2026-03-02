942120d8e57f2740a9737c4d6c40117a408b891f
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-02 23:16:46 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,12 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
 
+    from app.models.user import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
     from app.auth.routes import auth
     from app.main.routes import main
     from app import models
