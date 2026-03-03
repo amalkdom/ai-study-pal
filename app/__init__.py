15976d4b16827a84b7e53f3eb2702d6d1d876d45
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-03 19:20:00 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,27 +7,20 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
-
     app = Flask(__name__)
-
     app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
 
-    from app.models.user import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
     from app.auth.routes import auth
     from app.main.routes import main
-    from app import models
+    from app.admin.routes import admin
 
     app.register_blueprint(auth)
     app.register_blueprint(main)
+    app.register_blueprint(admin)
 
     with app.app_context():
         db.create_all()
