a8e14a66093bead59c280879245ccfcbf3756237
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-03 19:35:24 +0530

    Create question.py

---
+++ app/models/question.py
@@ -0,0 +1,15 @@
+from app import db
+from datetime import datetime
+
+class Question(db.Model):
+    __tablename__ = "questions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quizzes.id"))
+    text = db.Column(db.Text)
+    option_a = db.Column(db.String(255))
+    option_b = db.Column(db.String(255))
+    option_c = db.Column(db.String(255))
+    option_d = db.Column(db.String(255))
+    correct_answer = db.Column(db.String(10))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
