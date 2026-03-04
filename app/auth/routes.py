d930adb61429a7f174b5e2e41e9636a45b42d750
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-04 23:39:49 +0530

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,15 +1,28 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user
 from app.models.user import User
 from app import db
 
-# CREATE BLUEPRINT FIRST
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("main.dashboard"))
+
+        return "Invalid email or password"
+
     return render_template("login.html")
 
 
