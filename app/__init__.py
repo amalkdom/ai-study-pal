e6df21b6bd429df809eb9f194c44ddae8dac2eae
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-04 18:52:51 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
+
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = "auth.login"
 
-    from app.auth.routes import auth
     from app.main.routes import main
-    from app.admin.routes import admin
+    from app.auth.routes import auth
 
-    app.register_blueprint(auth)
     app.register_blueprint(main)
-    app.register_blueprint(admin)
+    app.register_blueprint(auth)
 
+    # CREATE DATABASE TABLES
     with app.app_context():
         db.create_all()
 
