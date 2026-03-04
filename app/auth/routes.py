f35a9a9026f0ad775aceab0c26fe2eeed3e70463
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-04 19:49:50 +0530

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,39 +1,29 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from werkzeug.security import generate_password_hash
-from flask_login import login_user
-from app.models.user import User
-from app import db
-
-auth = Blueprint("auth", __name__)
-
-
-@auth.route("/login")
-def login():
-    return render_template("login.html")
-
-
 @auth.route("/register", methods=["GET", "POST"])
 def register():
 
     if request.method == "POST":
 
         name = request.form.get("name")
+        age = request.form.get("age")
+        class_level = request.form.get("class_level")
         email = request.form.get("email")
         password = request.form.get("password")
 
         hashed_password = generate_password_hash(password)
 
-        user = User(
+        new_user = User(
             name=name,
+            age=age,
+            class_level=class_level,
             email=email,
             password=hashed_password
         )
 
-        db.session.add(user)
+        db.session.add(new_user)
         db.session.commit()
 
-        login_user(user)
+        login_user(new_user)
 
         return redirect(url_for("main.dashboard"))
 
-    return render_template("register.html")register.html")
+    return render_template("register.html")
