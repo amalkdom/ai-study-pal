0cdcea547bafe84ebae08b616701e8405799eaa4
amalkdom <jonaegon6jtargaryen@gmail.com>
2026-03-04 18:22:26 +0530

    Create xp_engine.py

---
+++ app/services/xp_engine.py
@@ -0,0 +1,13 @@
+def calculate_xp(score):
+
+    if score >= 90:
+        return 50
+
+    elif score >= 75:
+        return 30
+
+    elif score >= 60:
+        return 20
+
+    else:
+        return 10
